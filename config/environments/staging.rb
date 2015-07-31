1e79a6aedafa3ba78c798321bf2d202d742b0222
melissawahnish <melissawahnish@gmail.com>
2015-07-31 14:03:23 -0400

    Refactor sendgrid info

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -77,10 +77,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.smtp_settings =  {
     address:              'smtp.sendgrid.net',
-    domain:               'rubythursday.com',
+    domain:               'rubysnack.com',
     user_name:            ENV["EMAILUSERNAME"],
     password:             ENV["EMAILPASSWORD"],
     port:                 587,
